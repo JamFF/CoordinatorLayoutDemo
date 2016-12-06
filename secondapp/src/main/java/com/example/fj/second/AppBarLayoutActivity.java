46d5a4ea8746e77e013ac92b2e3a5657ade2fa9f
ffuujian <ffuujian@163.com>
2016-12-06 21:13:31 +0800

    添加TabLayout

--- secondapp/src/main/java/com/example/fj/second/AppBarLayoutActivity.java
+++ secondapp/src/main/java/com/example/fj/second/AppBarLayoutActivity.java
@@ -1,16 +1,58 @@
 package com.example.fj.second;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class AppBarLayoutActivity extends AppCompatActivity {
+import com.example.fj.second.adapter.MyPagerAdapter;
+import com.example.fj.second.dummy.DummyContent;
+import com.example.fj.second.fragment.ItemFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppBarLayoutActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
+
+    private TabLayout tab;
+
+    private ViewPager viewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_bar_layout);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tab = (TabLayout) findViewById(R.id.tab);
+        viewpager = (ViewPager) findViewById(R.id.viewpager);
+    }
+
+    private void initData() {
+        // viewpager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tab));
+
+        List<String> mList = new ArrayList<>();
+
+        if (mList.size() > 0) {
+            mList.clear();
+        }
+        mList.add("Tab 1");
+        mList.add("Tab 2");
+        mList.add("Tab 3");
+        mList.add("Tab 4");
+        mList.add("Tab 5");
+
+        viewpager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mList));
+        tab.setupWithViewPager(viewpager);
     }
 
     @Override
@@ -33,5 +75,10 @@ public class AppBarLayoutActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
---
+++ secondapp/src/main/java/com/example/fj/second/adapter/MyPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.fj.second.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.fj.second.fragment.ItemFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述：
+ * 作者：傅健
+ * 创建时间：2016/12/6 17:55
+ */
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    private List<String> mList = new ArrayList<>();
+
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public MyPagerAdapter(FragmentManager fm, List<String> list) {
+        super(fm);
+        mList = list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return ItemFragment.newInstance(0);
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mList.get(position);
+    }
+}
