6910919c30ff97213bcc9408fd11d44b99d4afb1
ffuujian <ffuujian@163.com>
2016-12-17 18:49:28 +0800

    完成CollapsingToolbarLayout效果

--- secondapp/src/main/java/com/example/fj/second/CollapsingToolbarLayoutActivity.java
+++ secondapp/src/main/java/com/example/fj/second/CollapsingToolbarLayoutActivity.java
@@ -1,7 +1,13 @@
 package com.example.fj.second;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class CollapsingToolbarLayoutActivity extends AppCompatActivity {
 
@@ -9,5 +15,68 @@ public class CollapsingToolbarLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapsing_toolbar_layout);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // 4.0以上Navigation默认false
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            // Title默认true
+            actionBar.setDisplayShowTitleEnabled(true);
+            // Logo默认true
+            actionBar.setDisplayUseLogoEnabled(true);
+        }
+
+        // 需要放在setSupportActionBar(toolbar)之后设置监听才有效果
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String msg = null;
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        msg = "Click setting";
+                        break;
+                }
+
+                if (msg != null) {
+                    Toast.makeText(CollapsingToolbarLayoutActivity.this, msg, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_collapsing, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
