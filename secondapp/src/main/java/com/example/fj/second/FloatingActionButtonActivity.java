084b9e6494dcdc347146282cf023dcfb279ae1d3
ffuujian <ffuujian@163.com>
2016-12-18 19:12:49 +0800

    菜单栏修改

--- secondapp/src/main/java/com/example/fj/second/FloatingActionButtonActivity.java
+++ secondapp/src/main/java/com/example/fj/second/FloatingActionButtonActivity.java
@@ -119,20 +119,4 @@ public class FloatingActionButtonActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_fab, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Toast.makeText(FloatingActionButtonActivity.this, getString(R.string.action_settings), Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
